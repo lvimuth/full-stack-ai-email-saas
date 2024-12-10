9de42b07bfc6025ca27e24cb434f6d9cf830bafd
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-12-10 15:27:37 +0400

    clerk installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
-import Link from "next/link";
-
-import { LatestPost } from "@/app/_components/post";
-import { api, HydrateClient } from "@/trpc/server";
 import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
   return (
     <div>
       <h1 className="text-red-600">Hello</h1>
